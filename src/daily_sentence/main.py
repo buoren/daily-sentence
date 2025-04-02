84989d18a8c52db34fdd0d55209b10cb0ef9b2e3
k3 <buoren@gmail.com>
2025-04-02 19:24:40 +0200

    Deal with dynamic port definitions

--- src/daily_sentence/main.py
+++ src/daily_sentence/main.py
@@ -1,5 +1,8 @@
 from nicegui import ui
 from daily_sentence.pages.index import main_page
+import os
+
+port = os.environ.get('PORT', 8080)
 
 main_page()
-ui.run()
+ui.run(host='0.0.0.0', port=port, title='Daily Sentence')
