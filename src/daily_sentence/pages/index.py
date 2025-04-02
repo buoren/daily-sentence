8eaf650ef84c325544e10b41797de11eba080754
k3 <buoren@gmail.com>
2025-04-02 22:53:27 +0200

    Clear inputs when a new constraint is set

--- src/daily_sentence/pages/index.py
+++ src/daily_sentence/pages/index.py
@@ -74,6 +74,8 @@ def main_page():
 
             def on_new_constraints():
                 nonlocal constraints, constraint_cards
+                result_container.clear()
+                user_input.clear()
                 constraints, constraint_cards = reconstitute_constraint_cards(constraint_cards)
 
             make_buttons()
